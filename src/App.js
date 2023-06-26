254c6d98a2e6ca0d4995ae3c402a08bd79500364
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-26 18:18:42 +0300

    Created react app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello FireDrive!
+            <p>- Stream and store your files with ease!</p>
+        </h1>
+    )
+}
