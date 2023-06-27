2582ffb207be9a9c5d7e16a637e5b14ce5138c1e
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-27 22:50:01 +0300

    Implement Home page
    Functioning: view current count of media

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import HeaderContainer from "./Header/HeaderContainer";
+import Home from "./Home";
+import {rootRoute} from "../common/commonData";
+import {useSelector} from "react-redux";
+import {useLocation} from "react-router-dom";
+import {connect} from "react-redux";
+import {setCurrentRoute} from "../redux/mediaSlice";
 
-const Main = () => {
+const Main = ({setCurrentRoute}) => {
+    const location = useLocation()
+    const pathName = location.pathname
+    const currentRoute = useSelector(state => state.media.currentRoute)
+    const homePage = currentRoute === rootRoute
+
+    useEffect(() => {
+        setCurrentRoute(pathName)
+    }, [pathName])
+
     return (
         <>
             <HeaderContainer/>
+            <main>
+                {homePage && <Home/>}
+            </main>
         </>
 
     );
 };
 
-export default Main;
+export default connect(null, {setCurrentRoute})(Main);
