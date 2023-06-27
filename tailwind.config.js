8b03af765bfa07fedfeeac6935cb817845c5c0cb
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-27 22:08:17 +0300

    Add store. Implement mediaSlice. Add commonData.js
    mediaSlice: Add functionality to fetch and upload Media

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            width: {
+                '300': '300px !important',
+            },
+            height: {
+                '300': '300px !important',
+            },
+        },
+    },
+    plugins: [],
 }
