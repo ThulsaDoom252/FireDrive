35453d6a52f2474cf818bb007c7dab2aa81e7219
ThulsaDoom252 <xenolm252@gmail.com>
2023-07-09 23:30:25 +0300

    Add styles to MediaOptions component

--- src/components/common/mediaOptions.js
+++ src/components/common/mediaOptions.js
@@ -5,8 +5,8 @@ import {BsPencilFill} from "react-icons/bs";
 
 const MediaOptions = ({handleMediaName, name}) => {
     return (
-        <div className={'hover:cursor-pointer'}>
-            <BsPencilFill onClick={() => handleMediaName({name})}/>
+        <div className={'hover:cursor-pointer p-1 h-5 bg-gray-300 flex justify-center items-center rounded'}>
+            <BsPencilFill color={'blue'} onClick={() => handleMediaName({name})}/>
         </div>
     );
 };
