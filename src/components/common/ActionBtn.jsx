cad0f14eec5f635658d4da0fa2863b5a5a457acb
ThulsaDoom252 <xenolm252@gmail.com>
2023-07-23 13:43:00 +0300

    implement clsx npm
    Added clsx to ActionBtn

--- src/components/common/ActionBtn.jsx
+++ src/components/common/ActionBtn.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from "clsx";
 
 const ActionBtn = ({label = 'button', icon, smallScreen, isFullWidth = false, isDisabled, handleClick}) => {
 
@@ -9,10 +10,17 @@ const ActionBtn = ({label = 'button', icon, smallScreen, isFullWidth = false, is
                     type="button"
                     disabled={isDisabled}
                     onClick={handleClick}
-                    className={`${isFullWidth && 'w-full'} bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed`}
+                    className={clsx(`
+                     bg-purple-500
+                      text-white 
+                      font-bold py-2
+                       px-4 rounded 
+                        hover:bg-purple-60`,
+                        isDisabled && 'opacity-50 cursor-not-allowed',
+                        isFullWidth && 'w-full',
+                    )}
                 >
                     {smallScreen ? icon : label}
-
                 </button>
             </>
         </>
