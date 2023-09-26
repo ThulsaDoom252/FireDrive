7e8aff234c835963c70ac17bc19686f234a3f900
ThulsaDoom252 <xenolm252@gmail.com>
2023-09-26 23:25:34 +0300

    added skeleton loader to modal video item

--- src/components/media/ModalVideoItem.jsx
+++ src/components/media/ModalVideoItem.jsx
@@ -1,6 +1,8 @@
 import React, {useRef, useState} from 'react';
 import ReactPlayer from "react-player";
 import {formatTime} from "../../common/commonData";
+import {ClipLoader} from "react-spinners";
+import {Skeleton} from "@mui/material";
 
 const ModalVideoItem = ({
                             item,
@@ -13,6 +15,7 @@ const ModalVideoItem = ({
     const videoRef = useRef(null)
     const [isPlaying, setIsPlaying] = useState(false)
     const [totalTime, setTotalTime] = useState(0)
+    const [isReady, setIsReady] = useState(false)
 
     const handleMouseEnter = () => {
         setIsPlaying(true);
@@ -23,8 +26,10 @@ const ModalVideoItem = ({
         videoRef.current.seekTo(0);
     };
 
-    const formatCurrentPlayerTime = () => {
+
+    const handleReadyModalItem = () => {
         setTotalTime(formatTime(videoRef.current.getDuration()))
+        setIsReady(true)
     }
 
     return (
@@ -44,13 +49,16 @@ const ModalVideoItem = ({
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
                 className={` relative w-full h-full  bg-black rounded ${item.url === currentModalItemUrl && 'border-4 border-amber-300'} flex items-center justify-center cursor-pointer`}>
-                <ReactPlayer onReady={formatCurrentPlayerTime}
+                {!isReady && <div className={'absolute inset-0 flex items-center justify-center'}>
+                    <ClipLoader size={50} color={'white'}/>
+                </div>}
+                <ReactPlayer onReady={handleReadyModalItem}
                              volume={0}
                              playing={isPlaying}
                              ref={videoRef} url={item?.url || ''} height={'98%'}
                              width={'98%'}/>
                 <div
-                    className='absolute bottom-0 left-2 text-white'>{totalTime}</div>
+                    className='absolute bottom-0 left-2 text-white'>{isReady && totalTime}</div>
             </div>
             <div className={`
             text-white
@@ -58,7 +66,8 @@ const ModalVideoItem = ({
             text-center
             ${info !== 'center' && 'mt-3'}
             
-            `}>{item?.name || ''}</div>
+            `}>{isReady ? item.name :
+                <Skeleton animation={'wave'} variant="rectangular" style={{width: '100%'}} height={10}/>}</div>
         </div>
     );
 };
