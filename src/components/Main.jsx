b4471e7169cb3145e71b5525084f62bc87d872e0
ThulsaDoom252 <xenolm252@gmail.com>
2023-10-15 21:23:43 +0300

    Refactored burger menu in seperate component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,7 +19,7 @@ import MediaContainer from "./media/MediaContainer";
 import {
     setItemModalType, setModalType, setMountedItemModal, setMountedModal, toggleCurrentTheme, toggleListMode,
 } from "../redux/appSlice";
-import BurgerMenu from "./common/BurgerMenu";
+import BurgerMenuWrapper from "./common/BurgerMenuWrapper";
 import SortInput from "./common/SortInput";
 import AudioPlayer from "./audioPlayer/AudioPlayer";
 import {PaginatorContext} from "../context/PaginatorContext";
@@ -47,6 +47,7 @@ import useConfirm from "./hooks/useConfirm";
 import {useStyles} from "./mui/styles";
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import toast from "react-hot-toast";
+import BurgerMenu from "./common/BurgerMenu";
 
 const Main = ({
                   currentMediaSet,
@@ -232,108 +233,26 @@ const Main = ({
                 classes={classes}
             />
             <main className={'w-full h-full overflow-y-scroll relative'} id={mainContentId} onClick={hideMobileSearch}>
-                <BurgerMenu smallScreen={smallScreen} onClick={hideMobileSearch}>
-                    <div className={'mt-5 flex flex-col justify-center'}>
-                        <div onClick={() => setModalType(userModal)} className={'mb-5 mx-auto'}><UserAvatar
-                        /></div>
-
-
-                        <div className={'w-full flex justify-center items-center flex-col'}>
-                            {isMediaLoading &&
-                                <>
-                                    <div>{uploadProgress}</div>
-                                    <input type={'range'}
-                                           value={totalUploadedBytes}
-                                           min={0}
-                                           max={totalBytesToUpload}/>
-                                </>
-                            }
-                        </div>
-                        <div className={'flex justify-between items-center mb-2'}>
-                            <div className={'mx-auto w-40%'}><UploadContainer/></div>
-                            <div className={'mx-auto w-40%'}><RemoveAllBtnContainer confirm={confirm}/></div>
-                        </div>
-                        <div className={'bg-gray-100 h-0.5 rounded w-full'}/>
-
-                        <div className='mt-3 mb-3'>
-                            <DropDownMenu menuType={isThemeBlockOpened} toggleMenu={setIsThemeBlockOpened}
-                                          btnLabel={'Change theme'} smallScreenIcon={<BiColorFill/>}>
-                                <AdaptiveImage
-                                    currentThemeName={currentThemeName}
-                                    theme={dayTheme}
-                                    url={DAS}
-                                    onClick={() => toggleCurrentTheme({type: dayTheme})}/>
-                                <AdaptiveImage
-                                    currentThemeName={currentThemeName}
-                                    theme={nightTheme}
-                                    url={NS}
-                                    onClick={() => toggleCurrentTheme({type: nightTheme})}/>
-                                <AdaptiveImage
-                                    currentThemeName={currentThemeName}
-                                    theme={desertTheme}
-                                    url={DS}
-                                    onClick={() => toggleCurrentTheme({type: desertTheme})}/>
-                            </DropDownMenu>
-                            <div className={'mt-3'}>
-                                <DropDownMenu
-                                    menuType={isSettingsBlockOpened} toggleMenu={setIsSettingsBlockOpened}
-                                    btnLabel={'Settings'} smallScreenIcon={<CiSettings/>}>
-                                    <div className={'bg-gray-600 bg-opacity-50 w-full flex flex-col'}>
-                                        <div className={`
-                                        w-full 
-                                        flex 
-                                        justify-between 
-                                        items-center 
-                                        mb-4 
-                                        mt-4
-                                        ${smallScreen ? 'flex-col' : ''}
-                                        `}>
-                                            <div className={'text-gray-400'}>List mode</div>
-                                            <div
-                                                className={'cursor-pointer'}
-                                                onClick={handleListMode}>{isPaginatorEnabled ? 'Pagination' : 'Lazy'}</div>
-                                        </div>
-                                        <div className={`
-                                        w-full 
-                                        flex 
-                                        justify-between 
-                                        items-center 
-                                        mb-4 
-                                        mt-4
-                                        ${smallScreen ? 'flex-col' : ''}
-                                        `}>
-                                            <div className={'text-gray-400 text-center'}>Items per page</div>
-                                            <div className={`flex 
-                                            flex-col 
-                                            justify-center 
-                                            items-center`}>
-                                                <div className={'text-white'}>{itemsPerPage}</div>
-                                                <input type={'range'} value={itemsPerPage} min={5} max={100}
-                                                       onChange={(e) =>
-                                                           setItemsPerPage(e.currentTarget.value)}/>
-                                            </div>
-                                        </div>
-                                        <div className={`
-                                        w-full 
-                                        flex 
-                                        justify-between 
-                                        items-center 
-                                        mb-4 
-                                        mt-4
-                                        ${smallScreen ? 'flex-col' : ''}
-                                        `}>
-                                            <div className={'text-gray-400'}>Animations</div>
-                                            <div>On</div>
-                                        </div>
-                                    </div>
-                                </DropDownMenu>
-                            </div>
-
-                        </div>
-                        <div className={'mb-5 '}><LogOutContainer/></div>
-                    </div>
-                    <div><SortInput/></div>
-                </BurgerMenu>
+                <BurgerMenu  {...{
+                    smallScreen,
+                    hideMobileSearch,
+                    setModalType,
+                    isMediaLoading,
+                    uploadProgress,
+                    totalUploadedBytes,
+                    totalBytesToUpload,
+                    confirm,
+                    isThemeBlockOpened,
+                    setIsThemeBlockOpened,
+                    currentThemeName,
+                    toggleCurrentTheme,
+                    isSettingsBlockOpened,
+                    setIsSettingsBlockOpened,
+                    handleListMode,
+                    isPaginatorEnabled,
+                    itemsPerPage,
+                    setItemsPerPage
+                }}/>
                 {homePage && <Home
                     smallScreen={smallScreen}
                     currentTheme={currentTheme}/>}
--- src/components/common/BurgerMenu.jsx
+++ src/components/common/BurgerMenu.jsx
@@ -1,33 +1,144 @@
-import {useContext} from "react";
-import {slide as Menu} from 'react-burger-menu'
-import {BurgerMenuContext} from "../../context/BurgerMenuContext";
-import {mainContentId, wrapperId} from "../../common/commonData";
+import React from 'react';
+import BurgerMenuWrapper from "./BurgerMenuWrapper";
+import userModal from "../modals/UserModal";
+import UserAvatar from "../user/UserAvatar";
+import UploadContainer from "../btns/UploadBtnContainer";
+import RemoveAllBtnContainer from "../btns/RemoveAllBtnContainer";
+import DropDownMenu from "./DropDownMenu";
+import {BiColorFill} from "react-icons/bi";
+import AdaptiveImage from "../AdaptiveImage";
+import {dayTheme, desertTheme, nightTheme} from "../../common/themes";
+import DAS from "../../images/themeTypes/DAS.jpg";
+import NS from "../../images/themeTypes/NS.jpg";
+import DS from "../../images/themeTypes/DS.jpg";
+import {CiSettings} from "react-icons/ci";
+import LogOutContainer from "../btns/LogOutContainer";
+import SortInput from "./SortInput";
 
 const BurgerMenu = ({
                         smallScreen,
-                        adaptForASmallScreen = true,
-                        pageWrapId = wrapperId, contentId = mainContentId,
-                        children,
-                        onClick,
+                        hideMobileSearch,
+                        setModalType,
+                        isMediaLoading,
+                        uploadProgress,
+                        totalUploadedBytes,
+                        totalBytesToUpload,
+                        confirm,
+                        isThemeBlockOpened,
+                        setIsThemeBlockOpened,
+                        currentThemeName,
+                        toggleCurrentTheme,
+                        isSettingsBlockOpened,
+                        setIsSettingsBlockOpened,
+                        handleListMode,
+                        isPaginatorEnabled,
+                        itemsPerPage,
+                        setItemsPerPage
                     }) => {
-    const ctx = useContext(BurgerMenuContext)
-    const {menuOpenState} = ctx
-
     return (
-        <Menu
-            right
-            pageWrapId={pageWrapId}
-            outerContainerId={contentId}
-            customBurgerIcon={false}
-            width={adaptForASmallScreen && smallScreen ? '35%' : void 0}
-            isOpen={menuOpenState}
-        >
-            <div className={'w-full flex flex-col justify-center pb-40'}>
-                {children}
+        <BurgerMenuWrapper smallScreen={smallScreen} onClick={hideMobileSearch}>
+            <div className={'mt-5 flex flex-col justify-center'}>
+                <div onClick={() => setModalType(userModal)} className={'mb-5 mx-auto'}><UserAvatar
+                /></div>
+
+
+                <div className={'w-full flex justify-center items-center flex-col'}>
+                    {isMediaLoading &&
+                        <>
+                            <div>{uploadProgress}</div>
+                            <input type={'range'}
+                                   value={totalUploadedBytes}
+                                   min={0}
+                                   max={totalBytesToUpload}/>
+                        </>
+                    }
+                </div>
+                <div className={'flex justify-between items-center mb-2'}>
+                    <div className={'mx-auto w-40%'}><UploadContainer/></div>
+                    <div className={'mx-auto w-40%'}><RemoveAllBtnContainer confirm={confirm}/></div>
+                </div>
+                <div className={'bg-gray-100 h-0.5 rounded w-full'}/>
+
+                <div className='mt-3 mb-3'>
+                    <DropDownMenu menuType={isThemeBlockOpened} toggleMenu={setIsThemeBlockOpened}
+                                  btnLabel={'Change theme'} smallScreenIcon={<BiColorFill/>}>
+                        <AdaptiveImage
+                            currentThemeName={currentThemeName}
+                            theme={dayTheme}
+                            url={DAS}
+                            onClick={() => toggleCurrentTheme({type: dayTheme})}/>
+                        <AdaptiveImage
+                            currentThemeName={currentThemeName}
+                            theme={nightTheme}
+                            url={NS}
+                            onClick={() => toggleCurrentTheme({type: nightTheme})}/>
+                        <AdaptiveImage
+                            currentThemeName={currentThemeName}
+                            theme={desertTheme}
+                            url={DS}
+                            onClick={() => toggleCurrentTheme({type: desertTheme})}/>
+                    </DropDownMenu>
+                    <div className={'mt-3'}>
+                        <DropDownMenu
+                            menuType={isSettingsBlockOpened} toggleMenu={setIsSettingsBlockOpened}
+                            btnLabel={'Settings'} smallScreenIcon={<CiSettings/>}>
+                            <div className={'bg-gray-600 bg-opacity-50 w-full flex flex-col'}>
+                                <div className={`
+                                        w-full 
+                                        flex 
+                                        justify-between 
+                                        items-center 
+                                        mb-4 
+                                        mt-4
+                                        ${smallScreen ? 'flex-col' : ''}
+                                        `}>
+                                    <div className={'text-gray-400'}>List mode</div>
+                                    <div
+                                        className={'cursor-pointer'}
+                                        onClick={handleListMode}>{isPaginatorEnabled ? 'Pagination' : 'Lazy'}</div>
+                                </div>
+                                <div className={`
+                                        w-full 
+                                        flex 
+                                        justify-between 
+                                        items-center 
+                                        mb-4 
+                                        mt-4
+                                        ${smallScreen ? 'flex-col' : ''}
+                                        `}>
+                                    <div className={'text-gray-400 text-center'}>Items per page</div>
+                                    <div className={`flex 
+                                            flex-col 
+                                            justify-center 
+                                            items-center`}>
+                                        <div className={'text-white'}>{itemsPerPage}</div>
+                                        <input type={'range'} value={itemsPerPage} min={5} max={100}
+                                               onChange={(e) =>
+                                                   setItemsPerPage(e.currentTarget.value)}/>
+                                    </div>
+                                </div>
+                                <div className={`
+                                        w-full 
+                                        flex 
+                                        justify-between 
+                                        items-center 
+                                        mb-4 
+                                        mt-4
+                                        ${smallScreen ? 'flex-col' : ''}
+                                        `}>
+                                    <div className={'text-gray-400'}>Animations</div>
+                                    <div>On</div>
+                                </div>
+                            </div>
+                        </DropDownMenu>
+                    </div>
+
+                </div>
+                <div className={'mb-5 '}><LogOutContainer/></div>
             </div>
-        </Menu>
-    )
-}
+            <div><SortInput/></div>
+        </BurgerMenuWrapper>
+    );
+};
 
-
-export default BurgerMenu
+export default BurgerMenu;
